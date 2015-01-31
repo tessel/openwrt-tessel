1f5e095929212d61b0717245535152479fcebdea
Kevin Mehall <km@kevinmehall.net>
2015-01-31 00:16:18 +0000

    Disable rt2800-pci. Unused on SoC, and contains binary blob firmware.

--- target/v2/config.mk
+++ target/v2/config.mk
@@ -4,4 +4,7 @@ CONFIG_TARGET_ramips=y
 CONFIG_TARGET_ramips_mt7620=y
 CONFIG_TARGET_ramips_mt7620_Default=y
 
+CONFIG_PACKAGE_kmod-rt2800-pci=n
+CONFIG_PACKAGE_kmod-rt2x00-pci=n
+
 PACKAGES+=kmod-spi-dev
