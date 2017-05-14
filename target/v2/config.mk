08af61f53afb26ea9022a987c0a720113210eba5
Kevin Mehall <km@kevinmehall.net>
2017-05-14 16:45:48 -0400

    Update to LEDE 17.01.1 and Node 6.10.3

--- target/v2/config.mk
+++ target/v2/config.mk
@@ -2,11 +2,12 @@ PROFILE=Tessel
 
 CONFIG_TARGET_ramips=y
 CONFIG_TARGET_ramips_mt7620=y
-CONFIG_TARGET_ramips_mt7620_Default=y
+CONFIG_TARGET_PROFILE="DEVICE_tessel"
+CONFIG_TARGET_ramips_mt7620_DEVICE_tessel=y
 
 CONFIG_PACKAGE_kmod-rt2800-pci=n
 CONFIG_PACKAGE_kmod-rt2x00-pci=n
 
 PACKAGES+=kmod-spi-dev
 PACKAGES+=tessel-tools
-PACKAGES+=uboot-mt7620-Default
+#PACKAGES+=uboot-mt7620-Default
